70ece247b4d0b5d270e52ce489a12688c416d2c5
Bryan <bryanpf93@gmail.com>
2026-04-01 15:25:45 +0200

    fix property level

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -17,7 +17,7 @@ const recipeSchema = new Schema({
     },
     level: {
         type: String,
-        enum: ["Easy Peasy", "Amateur", "Chef", "UltraPro Chef"]
+        enum: ["Easy Peasy", "Amateur Chef", "UltraPro Chef"]
     },
     ingredients: {
         type: [String]
